20de803feca1d95e9c058f9a1dfa1b8eca25cd9b
jushi <jushi@taobao.com>
2011-06-05 17:42:21 +0800

    clean code.

--- contributes/hs4j-kit/src/main/java/com/github/zhongl/hs4j/kit/arguments/StaticCollectorFactory.java
+++ contributes/hs4j-kit/src/main/java/com/github/zhongl/hs4j/kit/arguments/StaticCollectorFactory.java
@@ -16,39 +16,39 @@ public class StaticCollectorFactory {
   private static final int ENTITY_INDEX = 0;
   private static final int PRIMARY_INDEX = 0;
 
-  public static Collector<String[]> createKeysCollectorBy(ParameterAnnotations locator) {
-    final Method method = locator.getMethod();
+  public static Collector<String[]> createKeysCollectorBy(ParameterAnnotations annotations) {
+    final Method method = annotations.getMethod();
     final EntityClass entityClass = method.getAnnotation(EntityClass.class);
     if (entityClass != null) { // to find parameter type is entity class.
-      assertNotBothEntityClassAndOperatorAreAnnotatedBy(locator);
+      assertNotBothEntityClassAndOperatorAreAnnotatedBy(annotations);
       assertParameterTypeJustOnlyBeEntityClassOn(method);
       return new EntityKeysCollector(ENTITY_INDEX, PRIMARY_INDEX);
     }
-    return new KeysCollector(locator.getOnlyOneParamterIndexAnnotatedBy(Operator.class));
+    return new KeysCollector(annotations.getOnlyOneParamterIndexAnnotatedBy(Operator.class));
   }
 
-  private static void assertNotBothEntityClassAndOperatorAreAnnotatedBy(ParameterAnnotations locator) {
-    if (!locator.getAnnotationsOf(Operator.class).isEmpty())
+  private static void assertNotBothEntityClassAndOperatorAreAnnotatedBy(ParameterAnnotations annotations) {
+    if (!annotations.getAnnotationsOf(Operator.class).isEmpty())
       throw new IllegalArgumentException("Either " + EntityClass.class + " or " + Operator.class + " can be annotated");
   }
 
-  public static Collector<Integer> createLimitCollectorBy(ParameterAnnotations locator) {
-    return new LimitCollector(locator.getOnlyOneMoreParamterIndexAnnotatedBy(Limit.class));
+  public static Collector<Integer> createLimitCollectorBy(ParameterAnnotations annotations) {
+    return new LimitCollector(annotations.getOnlyOneMoreParamterIndexAnnotatedBy(Limit.class));
   }
 
-  public static Collector<Integer> createOffsetCollectorBy(ParameterAnnotations locator) {
-    return new OffsetCollector(locator.getOnlyOneMoreParamterIndexAnnotatedBy(Offset.class));
+  public static Collector<Integer> createOffsetCollectorBy(ParameterAnnotations annotations) {
+    return new OffsetCollector(annotations.getOnlyOneMoreParamterIndexAnnotatedBy(Offset.class));
   }
 
-  public static Collector<String[]> createValuesCollectorFrom(ParameterAnnotations locator) {
-    final Method method = locator.getMethod();
+  public static Collector<String[]> createValuesCollectorFrom(ParameterAnnotations annotations) {
+    final Method method = annotations.getMethod();
     final EntityClass entityClass = method.getAnnotation(EntityClass.class);
     if (entityClass != null) { // to find parameter type is entity class.
       assertParameterTypeJustOnlyBeEntityClassOn(method);
       final boolean excludePrimary = method.getAnnotation(HandlerSocket.class).value() != Action.INSERT;
       return new EntityValuesCollector(ENTITY_INDEX, PRIMARY_INDEX, excludePrimary);
     }
-    return new ArgumentsValuesCollector(locator.getAnnotationlessParameterIndexs());
+    return new ArgumentsValuesCollector(annotations.getAnnotationlessParameterIndexs());
   }
 
   private static void assertParameterTypeJustOnlyBeEntityClassOn(final Method method) {
