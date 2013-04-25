cdd529dc8766b8bd4d4429e155dace7398a80a8e
Shashy Dass <ShashyD@gmail.com>
2013-04-25 09:18:36 -0400

    Added two new methods to override the corresponding methods in the interface java.sql.resultSet in java 7.

--- src/main/java/com/google/code/hs4j/impl/ResultSetImpl.java
+++ src/main/java/com/google/code/hs4j/impl/ResultSetImpl.java
@@ -1,12 +1,12 @@
 /**
  *Copyright [2010-2011] [dennis zhuang(killme2008@gmail.com)]
  *Licensed under the Apache License, Version 2.0 (the "License");
- *you may not use this file except in compliance with the License. 
- *You may obtain a copy of the License at 
- *             http://www.apache.org/licenses/LICENSE-2.0 
- *Unless required by applicable law or agreed to in writing, 
- *software distributed under the License is distributed on an "AS IS" BASIS, 
- *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ *you may not use this file except in compliance with the License.
+ *You may obtain a copy of the License at
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *Unless required by applicable law or agreed to in writing,
+ *software distributed under the License is distributed on an "AS IS" BASIS,
+ *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  *either express or implied. See the License for the specific language governing permissions and limitations under the License
  */
 package com.google.code.hs4j.impl;
@@ -43,7 +43,7 @@ import java.util.TimeZone;
 /**
  * A java.sql.ResultSet implementation,most methods are not supported,use
  * getString as much as possible.
- * 
+ *
  * @author dennis
  * @date 2010-11-28
  */
@@ -520,6 +520,10 @@ public class ResultSetImpl implements ResultSet {
 		throw new UnsupportedOperationException();
 	}
 
+  public <T> T getObject(int columnIndex, Class<T> type) throws SQLException {
+    throw new UnsupportedOperationException();
+  }
+
 	public Object getObject(int columnIndex) throws SQLException {
 		throw new UnsupportedOperationException();
 	}
@@ -528,6 +532,10 @@ public class ResultSetImpl implements ResultSet {
 			throws SQLException {
 		throw new UnsupportedOperationException();
 	}
+
+  public <T> T getObject(String colName, Class<T> type) throws SQLException {
+    throw new UnsupportedOperationException();
+  }
 
 	public Object getObject(String columnName) throws SQLException {
 		throw new UnsupportedOperationException();
